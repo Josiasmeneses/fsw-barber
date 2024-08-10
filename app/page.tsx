99a9a7e75ce0d7d57a36585e34e97e02ae35f5f3
Josias Meneses <84454260+Josiasmeneses@users.noreply.github.com>
2024-08-10 16:10:34 -0300

    fix: add header and header componnents and banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,36 @@
 "use-client";
 
-import { useState } from 'react';
-import { Button } from './_components/ui/button';
+// import { useState } from 'react';
+
+import Header from "./_components/header";
+import { Input } from "./_components/ui/input";
+import { Button } from "./_components/ui/button";
+import { SearchIcon } from "lucide-react";
+import Image from "next/image";
 
 const Home = () => {
-  return <Button>Teste</Button>
-
-}
+  return (
+    <div>
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Josias!</h2>
+        <p>09/08/2024</p>
+        <div className="flex items-center gap-3">
+          <Input placeholder="Buscar Barbearia/Serviços" />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+        <div className="relative mt-6 w-full h-[150px]">
+          <Image
+            alt="Agende agora"
+            src="/Banner Pizza.png"
+            fill
+            className="object-cover rounded-xl"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 export default Home;
