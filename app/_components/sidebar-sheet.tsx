3b3051ce6d8db8e347191b2e4a1d9b42482ec80d
Josias Meneses <84454260+Josiasmeneses@users.noreply.github.com>
2024-08-13 17:22:47 -0300

    chore: add next auth setup

--- app/_components/sidebar-sheet.tsx
+++ app/_components/sidebar-sheet.tsx
@@ -1,10 +1,18 @@
 import { Button } from "./ui/button";
-import { CalendarIcon, HomeIcon, LogOutIcon } from "lucide-react";
+import { CalendarIcon, HomeIcon, LogInIcon, LogOutIcon } from "lucide-react";
 import { SheetClose, SheetContent, SheetHeader, SheetTitle } from "./ui/sheet";
 import { quickSearchOptions } from "../_constants/search";
 import Link from "next/link";
 import Image from "next/image";
-import { Avatar, AvatarImage } from "./ui/avatar";
+// import { Avatar, AvatarImage } from "./ui/avatar";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
 const SidebarSheet = () => {
   return (
     <SheetContent>
@@ -12,15 +20,37 @@ const SidebarSheet = () => {
         <SheetTitle className="text-left">Menu</SheetTitle>
       </SheetHeader>
 
-      <div className="flex items-center py-5 border-b border-solid">
-        <Avatar>
+      <div className="flex items-center justify-between py-5 border-b border-solid">
+        <h2 className="font-bold">Olá faça seu login!</h2>
+        <Dialog>
+          <DialogTrigger>
+            <Button size="icon">
+              <LogInIcon />
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="w-[90%]">
+            <DialogHeader>
+              <DialogTitle>Faça seu Login na plataforma</DialogTitle>
+              <DialogDescription>Conecte-se usando o google</DialogDescription>
+            </DialogHeader>
+            <Button variant="outline" className="gap-2 font-bold">
+              <Image
+                src="/google.svg"
+                width={18}
+                height={18}
+                alt="Conectar com o Google"
+              />
+            </Button>
+          </DialogContent>
+        </Dialog>
+        {/* <Avatar>
           <AvatarImage src="" />
         </Avatar>
 
         <div>
-          <p className="font-bold"></p>
-          <p className="text-gray-500 text-xs"></p>
-        </div>
+          <p className="font-bold">Josias</p>
+          <p className="text-gray-500 text-xs">Josias@gmail.com</p>
+        </div> */}
       </div>
 
       <div className="flex flex-col p-5 gap-2 py-5 border-b border-solid">
